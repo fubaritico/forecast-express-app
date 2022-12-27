16a45717067024d266007267d54ead1aafe61e34
Stéphane Cottereau <scottereau@greenflex.com>
2022-12-27 22:51:19 +0100

    feat: added eslint

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,26 +1,25 @@
-var router = require('express').Router();
-
+const router = require('express').Router();
 
 // http://localhost:9000/api/
-router.get('/', function(req, res){
-    res.status(200).send('All is fine')
+router.get('/', (req, res) => {
+  res.status(200).send('All is fine');
 });
 
 // http://localhost:9000/api/weather and some dynamic params
 router.use('/weather', require('./weather'));
 
-router.use(function(err, req, res, next){
-    if(err.name === 'ValidationError'){
-        return res.status(422).json({
-            errors: Object.keys(err.errors).reduce(function(errors, key){
-                errors[key] = err.errors[key].message;
+router.use((err, req, res, next) => {
+  if (err.name === 'ValidationError') {
+    return res.status(422).json({
+      errors: Object.keys(err.errors).reduce((errors, key) => {
+        errors[key] = err.errors[key].message;
 
-                return errors;
-            }, {})
-        });
-    }
+        return errors;
+      }, {}),
+    });
+  }
 
-    return next(err);
+  return next(err);
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,4 @@ const router = require('express').Router();
 
 router.use('/api', require('./api'));
 
-router.use('/', function(err, req, res, next){
-
-});
-
 module.exports = router;
