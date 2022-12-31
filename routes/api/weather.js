1bb1b49c8f3b44d276474ab63019e4f5ad5b6729
Stéphane Cottereau <scottereau@greenflex.com>
2022-12-31 20:37:09 +0100

    feat: added request handing for API external call (usefull, forward)

--- routes/api/weather.js
+++ routes/api/weather.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const https = require('https');
 
 // Preload user profile on routes with ':paramName',
 // see: https://github.com/gothinkster/node-express-realworld-example-app/blob/master/routes/api/profiles.js
@@ -17,14 +18,52 @@ router.param('secondParam', (req, res, next, secondParam) => {
   return next();
 });
 
+// http://localhost:9000/api/weather
+router.get('/', (req, res) => {
+  console.log(req.query);
+  return res.send(req.query);
+});
+
+function toQueryString(query) {
+  return Object.keys(query).reduce((qs, key, currentIndex) => `${qs}${key}=${query[key]}${currentIndex !== Object.keys(query).length - 1 ? '&' : ''}`, '?');
+}
+
+// http://localhost:9000/api/weather/get
+router.get('/get', (req, res) => {
+  const { query } = req;
+  const options = {
+    hostname: 'weatherbit-v1-mashape.p.rapidapi.com',
+    path: `/forecast/3hourly${toQueryString(query)}`,
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '1d75847856msha3c8730bcadede3p1de86bjsn6162510198bc',
+      'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com',
+    },
+  };
+
+  https.request(options, (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    response.on('end', () => res.send(data));
+  }).on('error', (err) => {
+    console.error('Error: ', err);
+    return res.status(500).send(err);
+  }).end();
+});
+
 // http://localhost:9000/api/weather/[:paramName]
-router.get('/:paramName', (req, res, next) => {
+router.get('/:paramName', (req, res) => {
   console.log(req.params);
+  console.log(req.query);
   return res.send(req.paramName);
 });
 
 // http://localhost:9000/api/weather/[:paramName]/[:secondParam]
-router.get('/:paramName/:secondParam', (req, res, next) => {
+router.get('/:paramName/:secondParam', (req, res) => {
   console.log(req.params);
   return res.json(req.params);
 });
